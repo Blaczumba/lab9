c8f9a57c8787e849a84394a70dfa283b8750cc77
Mateusz Modrzejewski <ma.modrzejewski@gmail.com>
2020-12-16 08:25:25 +0100

    Add exercises for PIPR lab 10

---
+++ classes_io.py
@@ -0,0 +1,20 @@
+from classes import Person, Database
+
+import csv
+import yaml
+
+
+class MalformedCSVError(Exception):
+    pass
+
+
+class CSVDataError(Exception):
+    pass
+
+
+def read_database_from_file(handle):
+    pass
+
+
+def write_database_to_file(handle, database):
+    pass
