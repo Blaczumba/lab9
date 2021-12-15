116d6cf840f386f105f512d7d77fb5699be2ad1c
Błażej Ejzak <blazej_ejzak@o2.pl>
2021-12-15 19:09:17 +0100

    moje zadanie

--- classes.py
+++ classes.py
@@ -1,6 +1,68 @@
+from datetime import date
+import csv
+
+
 class Person:
-    pass
+    def __init__(self, id=None, name=None, birth_date=None, father=None, mother=None):
+        self._id = id
+        self._name = name
+        self._birth_date = date.fromisoformat(birth_date)
+        self._mother = mother
+        self._father = father
+        if mother is not None:
+            mom_children = mother.children()
+            mom_children.append(self)
+        if father is not None:
+            father_children = father.children()
+            father_children.append(self)
+        self._children = []
+
+    def set_father(self, person):
+        self._father = person
+        father_children = person.children()
+        if self not in father_children:
+            father_children.append(self)
+
+    def set_mother(self, person):
+        self._mother = person
+        mother_children = person.children()
+        if self not in mother_children:
+            mother_children.append(self)
+
+    def id(self):
+        return self._id
+
+    def name(self):
+        return self._name
+
+    def birth_date(self):
+        return self._birth_date
+
+    def mother(self):
+        return self._mother
+
+    def father(self):
+        return self._father
+
+    def children(self):
+        return self._children
 
 
 class Database:
-    pass
+    def __init__(self, people=None):
+        if people is None:
+            self._people = []
+        else:
+            self._people = people
+
+    def add_person(self, person):
+        if person not in self._people:
+            self._people.append(person)
+
+    def get_person_by_id(self, id):
+        for person in self._people:
+            if person.id() == id:
+                return person
+
+    def people(self):
+        return self._people
--- classes_io.py
+++ classes_io.py
@@ -11,8 +11,27 @@ class CSVDataError(Exception):
     pass
 
 
-def read_database_from_file(handle):
-    pass
+def read_database_from_file(file_handle):
+    reader = csv.DictReader(file_handle)
+    people = []
+    for row in reader:
+        id = row["id"]
+        name = row["name"]
+        birth_date = row["birth_date"]
+        me = Person(id, name, birth_date)
+        people.append(me)
+    data = Database(people)
+    file_handle.seek(0)
+    for row in reader:
+        my_id = row["id"]
+        father_id = row["father_id"]
+        mother_id = row["mother_id"]
+        father = data.get_person_by_id(father_id)
+        mother = data.get_person_by_id(mother_id)
+        me = data.get_person_by_id(my_id)
+        me.set_mother(mother)
+        me.set_father(father)
+    return data
 
 
 def write_database_to_file(handle, database):
